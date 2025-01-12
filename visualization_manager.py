3e6b9e773d26ffc4c30071a7629995b8e877e6d9
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2025-01-12 21:03:43 +0200

    created graphs

--- visualization_manager.py
+++ visualization_manager.py
@@ -57,7 +57,7 @@ class VisualizationManager:
                 print(f"\nGenerating {metric_type} metrics:")
 
                 try:
-                    for graph_num in [70]:#range(1,13):#[70]:
+                    for graph_num in (list(range(1, 13)) + [70]): #[70]: #range(1,13):
                         print(f"  Creating graph {graph_num}...")
                         visualizer = self.get_visualizer(graph_num)
                         plt = visualizer.create_graph(
